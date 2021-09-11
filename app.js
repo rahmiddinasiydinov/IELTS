ba78c6e22335f567b3cb6af0a1f986e97507ebff
rahmiddinasiydinov <rahmiddinasiydinov>
2021-09-11 14:47:50 +0500

    initial commit

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 let form = document.querySelector('.user-form');
-let final = document.querySelector('.final')
+let final = document.querySelector('.final');
+let warnText = document.querySelector('.form-text');
+
 form.addEventListener('submit', function(event){
     event.preventDefault();
     let listening = document.querySelector('.listening').value;
@@ -9,6 +11,20 @@ form.addEventListener('submit', function(event){
     let total = (parseFloat(listening) + parseFloat(reading) + parseFloat(writing) + parseFloat(speaking));
     console.log(total);
     let overall = (Math.round(total/2))/2;
-    final.textContent = overall;
+
+    if(listening <=9 && reading <= 9 && writing <= 9 && speaking <=9){
+        if(listening >=0 && reading >= 0 && writing >= 0 && speaking >=0){
+            final.textContent = overall; 
+        }
+        else{
+            warnText.textContent = 'IELTS scores are between 0 and 9 ';
+        } 
+    }
+    else{
+      
+       warnText.textContent = 'IELTS scores are between 0 and 9 ';
+        
+    }
+   
 
 })
